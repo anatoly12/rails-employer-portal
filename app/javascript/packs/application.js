50333aa95e33f59da73ed89b5ce2deb7bbcd8c38
Anatoly P <anatolypolicastroa@gmail.com>
2020-06-08 13:10:13 +0000

    Start working on sign in page, remove assets directory since we're using webpacker instead of sprockets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "../src/style.scss";
-
 require("@rails/ujs").start()
+
+import "../src/style.scss";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
