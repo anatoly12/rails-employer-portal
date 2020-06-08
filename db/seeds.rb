aab61cc8923313272f2f57f74b7633f3628c2c9e
Anatoly P <anatolypolicastroa@gmail.com>
2020-06-08 10:39:04 +0000

    Allow reusing credentials in seeds by using environment variables

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ first_name = Faker::Name.first_name
 user = User.create(
   first_name: first_name,
   last_name: Faker::Name.last_name,
-  email: Faker::Internet.safe_email(name: first_name),
-  password: Faker::Internet.password
+  email: ENV.fetch("EMPLOYER_EMAIL", Faker::Internet.safe_email(name: first_name)),
+  password: ENV.fetch("EMPLOYER_PASSWORD", Faker::Internet.password)
 )
 
 puts
