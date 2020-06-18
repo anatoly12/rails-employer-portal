e94a4e6f8d96f0fa0f8bcb5e4fbfa6c603bb332b
Anatoly P <anatolypolicastroa@gmail.com>
2020-06-18 14:48:10 +0000

    Connect to remote DB

---
+++ app/lib/employer_portal/remote_db.rb
@@ -0,0 +1,26 @@
+module EmployerPortal
+  class RemoteDb
+    class << self
+      attr_reader :db
+
+      def init
+        connect
+        create_or_replace_views
+      end
+
+    private
+
+      def connect
+        @db ||= Sequel.connect ENV["SYNC_DATABASE_URL"]
+        Rails.logger.info "Successfully connected to remote DB..."
+      rescue Sequel::DatabaseConnectionError
+        abort "Can't connect to remote DB, please check that SYNC_DATABASE_URL is correctly configured."
+      end
+
+      def create_or_replace_views
+        Rails.logger.info "Create or replace views..."
+        # TODO
+      end
+    end
+  end
+end
--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,1 +1,5 @@
 Sequel.database_timezone = :utc
+
+Rails.application.reloader.to_prepare do
+  ::EmployerPortal::RemoteDb.init
+end
