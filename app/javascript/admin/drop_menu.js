6a99d055eb6d88a94b1e1d0846395f18b1742668
Anatoly P <anatolypolicastroa@gmail.com>
2020-06-30 09:11:14 +0000

    Reformat JS code

--- app/javascript/admin/drop_menu.js
+++ app/javascript/admin/drop_menu.js
@@ -1,27 +1,30 @@
 window.toggleDD = (myDropMenu) => {
-  document.getElementById(myDropMenu).classList.toggle("invisible");
+  document.getElementById(myDropMenu).classList.toggle('invisible')
 }
 window.filterDD = (myDropMenu, myDropMenuSearch) => {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById(myDropMenuSearch);
-  filter = input.value.toUpperCase();
-  div = document.getElementById(myDropMenu);
-  a = div.getElementsByTagName("a");
+  var input, filter, ul, li, a, i
+  input = document.getElementById(myDropMenuSearch)
+  filter = input.value.toUpperCase()
+  div = document.getElementById(myDropMenu)
+  a = div.getElementsByTagName('a')
   for (i = 0; i < a.length; i++) {
     if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
+      a[i].style.display = ''
     } else {
-      a[i].style.display = "none";
+      a[i].style.display = 'none'
     }
   }
 }
-window.onclick = function(event) {
-  if (!event.target.matches('.drop-button') && !event.target.matches('.drop-search')) {
-    var dropdowns = document.getElementsByClassName("dropdownlist");
+window.onclick = function (event) {
+  if (
+    !event.target.matches('.drop-button') &&
+    !event.target.matches('.drop-search')
+  ) {
+    var dropdowns = document.getElementsByClassName('dropdownlist')
     for (var i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+      var openDropdown = dropdowns[i]
       if (!openDropdown.classList.contains('invisible')) {
-        openDropdown.classList.add('invisible');
+        openDropdown.classList.add('invisible')
       }
     }
   }
