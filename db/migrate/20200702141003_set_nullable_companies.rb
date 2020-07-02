f03b62c1601f144f18f0c85f767e2eb2e5ff5f94
Anatoly P <anatolypolicastroa@gmail.com>
2020-07-02 20:34:32 +0000

    Try to fix migration

--- db/migrate/20200702141003_set_nullable_companies.rb
+++ db/migrate/20200702141003_set_nullable_companies.rb
@@ -2,9 +2,11 @@
 
 Sequel.migration do
   change do
+    Sequel::Model.db.run "SET FOREIGN_KEY_CHECKS=0"
     alter_table :companies do
       set_column_allow_null :plan_id, false
       drop_column :remote_sync_at
     end
+    Sequel::Model.db.run "SET FOREIGN_KEY_CHECKS=1"
   end
 end
