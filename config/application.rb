b36550795da984004273659c51178ad431f8ae34
Anatoly P <anatolypolicastroa@gmail.com>
2020-07-14 21:46:47 +0000

    Use a config instead of an initializer

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module EmployerPortal
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+      html_tag.html_safe
+    }
 
     # Don't generate system test files.
     config.generators.system_tests = nil
