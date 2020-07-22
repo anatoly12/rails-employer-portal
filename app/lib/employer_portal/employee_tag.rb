0e791d6b044e9fd3991e84056cec800b37d19eba
Anatoly P <anatolypolicastroa@gmail.com>
2020-07-22 11:23:15 +0000

    Prevent creation of new employee tags unless allowed in backend

--- app/lib/employer_portal/employee_tag.rb
+++ app/lib/employer_portal/employee_tag.rb
@@ -12,21 +12,14 @@ class EmployerPortal::EmployeeTag
   end
 
   def find_or_create_tags(tags_before, tag_names)
-    if context.allowed_all_employee_tags?
-      tag_names.map do |tag_name|
-        EmployeeTag.find_or_create(
-          company_id: context.company_id,
-          name: tag_name,
-        )
-      end
+    if context.allowed_to_add_employee_tags?
+      tag_names.map { |tag_name| EmployeeTag.find_or_create company_id: context.company_id, name: tag_name }
+    elsif context.allowed_all_employee_tags?
+      EmployeeTag.where(company_id: context.company_id, name: tag_names).all
     else
       tags_before.reject do |tag|
         context.allowed_employee_tags.include? tag.id
-      end + EmployeeTag.where(
-        company_id: context.company_id,
-        id: context.allowed_employee_tags,
-        name: tag_names,
-      ).all
+      end + EmployeeTag.where(id: context.allowed_employee_tags, name: tag_names).all
     end
   end
 
