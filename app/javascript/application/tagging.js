d3f9d9f96bd3bae4349a7fe040e78778158ddc67
Anatoly P <anatolypolicastroa@gmail.com>
2020-07-28 07:07:02 +0000

    Mark purgecss comments as important

--- app/javascript/application/tagging.js
+++ app/javascript/application/tagging.js
@@ -2,7 +2,7 @@ import Tagify from '@yaireo/tagify'
 
 document.addEventListener('turbolinks:load', () => {
   document.querySelectorAll('input[data-tagging]').forEach((input) => {
-    const whitelist = (input.dataset.whitelist || '').split(',')
+    const whitelist = (input.dataset.whitelist || '').split(',').filter(s => s!='')
     const enforceWhitelist = input.dataset.enforceWhitelist != 'false'
     new Tagify(input, {
       whitelist,
